fe4c84e60691077720f17955d291e1d63385cf06
captivea-dla <damien.lagorce@captivea.com>
2022-01-10 11:08:55 +0000

    [ADD] Basic Module

---
+++ space_mission/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Space Mission',
+    
+    'summary' : """Spatial mission gestion""",
+    
+    'description': """
+    
+        Space Mission is a module to help:
+        - Plan the space mission
+        - Organize the spaceship crew
+        - Organize the resources on board
+    """,
+    
+    'author': 'captivea_dla',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Space',
+    'version': '0.0.1',
+    
+    'depends' : ['base'],
+    
+    'data' : [
+        
+    ],
+    
+    'demo': [
+        
+    ]
+}
